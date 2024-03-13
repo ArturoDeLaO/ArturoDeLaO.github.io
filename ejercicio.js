8660c275a689b501cefe3dd2e145d5bda0a1bd56
ArturoDeLaO <162398197+ArturoDeLaO@users.noreply.github.com>
2024-03-13 12:38:33 -0600

    Add files via upload

---
+++ ejercicio.js
@@ -0,0 +1,15 @@
+function cambiarColor()
+{
+   variable_album = document.getElementById("album");
+   console.log(variable_album);
+   variable_album.style.backgroundColor = "Red";
+}
+
+function cambiarTamano()
+{
+   variable_imagen = document.getElementsByClassName("imagen");
+   console.log(variable_imagen);
+   variable_imagen:Spiderman.style.width = "100px";
+   variable_imagen:Spiderman.style.height = "100px";
+
+}
